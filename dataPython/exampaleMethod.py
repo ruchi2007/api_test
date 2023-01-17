2da9406898e616f4ed7616953da2412cdc03c3f1
ruchi2007 <ruchi2007usa@gmail.com>
2023-01-17 01:00:51 -0500

    commit file

---
+++ dataPython/exampaleMethod.py
@@ -0,0 +1,41 @@
+from testMethod import verify_status_code_200
+
+verify_status_code_200()
+# from testMethod import test
+
+# test()
+
+from testMethod import *
+
+
+# test1()
+
+
+def exampal():
+    print("test print")
+
+
+exampal()
+
+
+def exampal():
+    return 123
+
+
+print(exampal())
+
+
+def exampal():
+    a, b = 123, 456
+    return a + b
+
+
+print(exampal())
+
+
+def exampal():
+    a, b = 123, 456
+    return a, b
+
+
+print(exampal())
