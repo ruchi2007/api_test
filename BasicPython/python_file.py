b6a3f6fa2fd7c9a9ed13e418f5652d538497cc06
ruchi2007 <ruchi2007usa@gmail.com>
2023-02-16 22:12:02 -0500

    add file

---
+++ BasicPython/python_file.py
@@ -0,0 +1,28 @@
+# data = {"a":5, "b":7, "c":10}
+# val = data.values()
+# max_val = max(val)
+# min_val = min(val)
+# print(min_val)
+# print(max_val)
+
+#
+# val = [1,2,3,2,4,5,3]
+# unique_val = []
+# duplicate_val = []
+#
+# for x in val:
+#     if x not in unique_val:
+#         unique_val.append(x)
+#     elif x not in duplicate_val:
+#         duplicate_val.append(x)
+# print(unique_val)
+# print(duplicate_val)
+
+#
+# val = "hello"
+# if val == "hello1":
+#     print("value is found")
+#
+# else:
+#     print("value is not found")
+
